43e4a4b04184c7072968b9ddb57bea701ca5e382
Academica <Academica@Academica-PC.fibertel.com.ar>
2023-08-05 18:20:56 -0300

    version final

--- src/main/java/com/mycompany/aeropuerto/GestorCheckin.java
+++ src/main/java/com/mycompany/aeropuerto/GestorCheckin.java
@@ -6,53 +6,60 @@ package com.mycompany.aeropuerto;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
- * @author Maria Elisa Mendez Cares
- * Legajo: 61921
- * Carrera: Profesorado de Informatica
+ * @author Maria Elisa Mendez Cares 
+ * Legajo: 61921 
+ * Carrera: Profesorado de Informatica 
  * Email: maria.mendez@est.fi.uncoma.edu.ar
  */
 
 public class GestorCheckin {
-        int cantPuestos;
-        int capacidadColaCheckin = 2;
-        BlockingQueue<Thread>[] colaCheckin;
 
-        public GestorCheckin(int cantPuestosCheckin) {
-            this.cantPuestos = cantPuestosCheckin;
-            colaCheckin = new BlockingQueue[cantPuestos];
-            for (int i = 0; i < this.cantPuestos; i++) {
-                colaCheckin[i] = new LinkedBlockingQueue<>(capacidadColaCheckin);
-            }
+    int cantPuestos;
+    int capacidadColaCheckin = 2;
+    BlockingQueue<Thread>[] colaCheckin;
+    Semaphore semAtencionCheckin[];
+
+    public GestorCheckin(int cantPuestosCheckin) {
+        this.cantPuestos = cantPuestosCheckin;
+        semAtencionCheckin = new Semaphore[cantPuestos];
+        colaCheckin = new BlockingQueue[cantPuestos];
+        for (int i = 0; i < this.cantPuestos; i++) {
+            colaCheckin[i] = new LinkedBlockingQueue<>(capacidadColaCheckin);
+            semAtencionCheckin[i] = new Semaphore(0);
         }
+    }
 
-        public void hacerChecking(int puestoCheckin) {
-            Thread pasajero = Thread.currentThread();
+    public void hacerChecking(int puestoCheckin) {
+        Thread pasajero = Thread.currentThread();
+        try {
+            colaCheckin[puestoCheckin].put(pasajero);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(GestorCheckin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("El " + pasajero.getName() + " está haciendo cola en el puesto de Checkin " + puestoCheckin);
+        try {
+            semAtencionCheckin[puestoCheckin].acquire();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(GestorCheckin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void atenderPuestoCheckin(int puestoCheckin) {
+        Thread pasajero = null;
+        Thread puesto = Thread.currentThread();
+        synchronized (puesto) {
             try {
-                colaCheckin[puestoCheckin].put(pasajero);
+                pasajero = colaCheckin[puestoCheckin].take(); // El siguiente pasajero en la cola de espera
             } catch (InterruptedException ex) {
                 Logger.getLogger(GestorCheckin.class.getName()).log(Level.SEVERE, null, ex);
             }
-            System.out.println("El " + pasajero.getName() + " está haciendo cola en el puesto de Checkin " + puestoCheckin);
-            //synchronized (pasajero) {
-                pasajero.suspend(); // suspender  al pasajero para que continúe en la cola de check-in              
-          //  }
+            semAtencionCheckin[puestoCheckin].release();
         }
-
-        public void atenderPuestoCheckin(int puestoCheckin) {
-            Thread pasajero = null;
-            Thread puesto = Thread.currentThread();
-            synchronized (puesto) {
-                try {
-                    pasajero = colaCheckin[puestoCheckin].take(); // El siguiente pasajero en la cola de espera
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(GestorCheckin.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                pasajero.resume(); // reanudar el hilo pasajero
-                //System.out.println("El " + pasajero.getName() + " hizo el Checkin en el puesto " + puestoCheckin);
-            }
-        }
+    }
 }
